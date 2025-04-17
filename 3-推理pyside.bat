fe5be89bdad73643d109718b60925eedafdff2b7
syaofox <syaofox@gmail.com>
2025-04-17 21:42:46 +0800

    chore: remove obsolete PySide6 files and clean up project structure
    Deleted several outdated files related to the PySide6 UI, including main application scripts and controller modules. This cleanup enhances project organization by removing unused components, reflecting a shift towards a more streamlined codebase.

--- 3-推理pyside.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set GRADIO_TEMP_DIR=%~dp0TEMP
-
-call conda activate GPTSoVits311
-python pyside_app.py
-pause 
--- pyside_ui/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-控制器包
-""" 
