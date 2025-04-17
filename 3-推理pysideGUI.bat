fc2c8be2c6f20169e97d04fc744ce6ec49b8d0f1
syaofox <syaofox@gmail.com>
2025-04-17 20:07:47 +0800

    chore: update batch files and requirements
    Removed the obsolete tkinter.bat file to streamline the project and updated the pysideGUI.bat file by removing the pause command. Additionally, added the pysrt package to requirements-fox.txt to support new functionality. These changes enhance project organization and dependency management.

--- 3-推理pysideGUI.bat
+++ 3-推理pysideGUI.bat
@@ -3,4 +3,3 @@ set GRADIO_TEMP_DIR=%~dp0TEMP
 
 call conda activate GPTSoVits311
 python gpt_sovits_gui.py
-pause 
--- 3-推理tkinter.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set GRADIO_TEMP_DIR=%~dp0TEMP
-
-call conda activate GPTSoVits311
-python .\tkinter_ui.py
-pause
